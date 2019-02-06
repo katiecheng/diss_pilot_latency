4cb09f9481e9245f82e667d82178b5a8c0ab2ecb
Katie Cheng <katie.cheng@gmail.com>
2019-02-06 14:23:11 -0800

    adding some stuff to experiment.js

---
+++ experiment.js
@@ -0,0 +1,11 @@
+
+// Shows slides. We're using jQuery here - the **$** is the jQuery selector function, which takes as input either a DOM element or a CSS selector string.
+function showSlide(id) {
+  // Hide all slides
+	$(".slide").hide();
+	// Show just the slide we want to show
+	$("#"+id).show();
+}
+
+// Show the instructions slide -- this is what we want subjects to see first.
+showSlide("instructions");
